56921682a42eb7ab230311cc2d9367d4979ea6b8
David Voinson <david@centiva.ca>
2026-06-15 01:00:02 -0400

    Refactor login view: simplify button structure by removing unnecessary div wrapper

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,14 +1,12 @@
 <x-filament-panels::page.simple>
-    <div class="flex w-full flex-col">
-        <x-filament::button
-            tag="a"
-            color="primary"
-            size="lg"
-            icon="heroicon-m-globe-alt"
-            href="{{ $this->getGoogleRedirectUrl() }}"
-            class="w-full justify-center"
-        >
-            {{ __('filament-google-workspace-auth::filament-google-workspace-auth.login.button') }}
-        </x-filament::button>
-    </div>
+    <x-filament::button
+        tag="a"
+        color="primary"
+        size="lg"
+        icon="heroicon-m-globe-alt"
+        href="{{ $this->getGoogleRedirectUrl() }}"
+        class="w-full justify-center"
+    >
+        {{ __('filament-google-workspace-auth::filament-google-workspace-auth.login.button') }}
+    </x-filament::button>
 </x-filament-panels::page.simple>
